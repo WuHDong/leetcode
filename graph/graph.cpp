e9385150bf7870740c924efd415e3539ce33b0a1
WuHD <CharlesKinx@outlook.com>
2024-01-18 12:37:58 +0800

    单词接龙未完成

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<iostream>
 #include<queue>
+#include<unordered_map>
 using namespace std;
 /**
  * LCR 105. 岛屿的最大面积
@@ -65,7 +66,9 @@ void bfsIsBipartite(vector<vector<int>>& graph,vector<int>& arr,int index,int cn
         }
     }
 }
-
+/**
+ * LCR 107. 01 矩阵
+*/
 vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
     int row = mat.size();
     int col = mat[0].size();
@@ -102,5 +105,62 @@ vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         }
     }
     return ans;
+}
+
+/**
+ * LCR 108. 单词接龙
+*/
+int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    bool isExist = false;
+    unordered_map<string,bool> visited;
+    for(string s : wordList) {
+        visited[s] = false;
+        if(s == endWord) {
+            isExist = true;
+        }
+    }
+
+    if(!isExist) {
+        return 0;
+    }
+
+    queue<string> q;
+    int ans = 1;
+    q.emplace(beginWord);
+
+    while(!q.empty()) {
+        string cur = q.front();
+        q.pop();
+        
+        for(string s : wordList) {
+            if(!visited[s] && isReachable(cur,s)) {
+                if(s == endWord) {
+                    return ans +1;
+                }
+                q.emplace(s);
+                visited[s] = true;
+            }
+        }
+        ans++;
+    }
+    
+    return ans;
+
+}
+
+bool isReachable(string begin,string end) {
+    if(begin.size() != end.size()) {
+        return false;
+    }
+    int cnt = 0;
+    for(int i = 0;i<begin.size();i++) {
+        if(begin[i] != end[i]) {
+            cnt++;
+        }
+        if(cnt > 1) {
+            return false;
+        }
+    }
+    return true;
 }
 
