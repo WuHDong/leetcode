f78017fb1b3ac391b68b2bf31796a2445d4c33f3
WuHD <CharlesKinx@outlook.com>
2024-02-25 10:27:14 +0800

    1944. 队列中可以看到的人数

--- daydayup/daydayup.cpp
+++ daydayup/daydayup.cpp
@@ -123,3 +123,58 @@ int candy(vector<int>& ratings) {
     }
     return ans;
 }
+
+/**
+ * LCR 183. 望远镜中最高的海拔
+*/
+vector<int> maxAltitude(vector<int>& heights, int limit) {
+    
+    deque<int> dq;
+    vector<int> ans;
+    int len = heights.size();
+    if(len == 0) {
+        return ans;
+    }
+    for(int i = 0;i<limit;i++) {
+        while(!dq.empty() && heights[dq.back()] < heights[i]) {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+    }
+    ans.push_back(heights[dq.front()]);
+    for(int i = limit; i < len; i++) {
+        while(!dq.empty() && heights[dq.back()] < heights[i]) {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+
+        while(dq.front() <= i - limit) {
+            dq.pop_front();
+        }
+        ans.push_back(heights[dq.front()]);
+    }
+    return ans;
+} 
+
+/**
+ * 1944. 队列中可以看到的人数
+*/
+vector<int> canSeePersonsCount(vector<int>& heights) {
+    int len = heights.size();
+    vector<int> ans(len,0);
+    vector<int> st;
+
+    for(int i = len -1;i>=0;i--) {
+        int t = 0;
+        while(!st.empty() && heights[i] > st.back()) {
+            st.pop_back();
+            t++;
+        }
+        if(!st.empty()) {
+            t++;
+        }
+        ans[i] = t;
+        st.push_back(heights[i]);
+    }
+    return ans;
+}
