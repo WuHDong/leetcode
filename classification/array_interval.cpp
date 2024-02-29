2b77525f813678a3f68a0fcb8bc395d3dffebd03
WuHD <CharlesKinx@outlook.com>
2024-02-29 10:14:47 +0800

    1235. 规划兼职工作

--- classification/array_interval.cpp
+++ classification/array_interval.cpp
@@ -54,3 +54,35 @@ int findBinary(vector<vector<int>>& rides,int high,int target) {
     }
     return low;
 }
+
+/**
+ * 1235. 规划兼职工作 
+ * 
+ * 你打算利用空闲时间来做兼职工作赚些零花钱。
+ * 这里有 n 份兼职工作，每份工作预计从 startTime[i] 开始到 endTime[i] 结束，报酬为 profit[i]。
+ * 给你一份兼职工作表，包含开始时间 startTime，结束时间 endTime 和预计报酬 profit 三个数组
+ * 请你计算并返回可以获得的最大报酬。
+ * 注意，时间上出现重叠的 2 份工作不能同时进行。
+ * 如果你选择的工作在时间 X 结束，那么你可以立刻进行在时间 X 开始的下一份工作。
+*/
+
+int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+    int len = startTime.size();
+    vector<vector<int>> arr(len,vector<int>(3));
+
+    for(int i = 0;i<len;i++) {
+        arr[i][0] = startTime[i];
+        arr[i][1] = endTime[i];
+        arr[i][2] = profit[i];
+    }
+
+    sort(arr.begin(),arr.end(),cmp);
+
+    vector<int> dp(len+1);
+    for(int i = 0;i<len;i++) {
+        int j = findBinary(arr,i,arr[i][0]);
+        dp[i+1] = max(dp[i],dp[j]+arr[i][2]);
+    }
+
+    return dp[len];
+}
